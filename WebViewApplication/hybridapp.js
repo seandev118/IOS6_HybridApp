f962970ac9b9f095b000453fcb32ef71dedb14cb
Sean <sean@Seans-MacBook-Pro.local>
2014-01-27 14:52:18 -0800

    first

---
+++ WebViewApplication/hybridapp.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+                  
+                  $('#mem').hide().fadeIn(4000,
+                                          function() {
+                                          $( "span" ).fadeIn( 100 );
+                                          });
+                  
+                  
+                  
+                  $('#textbx').hide().fadeIn(4000,
+                                             function() {
+                                             $( "span" ).fadeIn( 100 );
+                                             });
+                  
+                  });
+
